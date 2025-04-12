0cb78375f596ad7ebf5d1c143e4d12a9591e9840
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-04-12 21:04:38 +0530

    TRY

--- backend/server.js
+++ backend/server.js
@@ -74,7 +74,7 @@ io.on("connection", (socket) => {
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
 	
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
