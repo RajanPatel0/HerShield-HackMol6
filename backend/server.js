2d5e5e0072417db5ec21fa0a3fefcd92d57b5fa1
Rajan Patel0 <rajanpatel.ias@gmail.com>
2025-04-12 23:07:39 +0530

    lastly

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,9 @@ import geminiRoutes from "./routes/geminiRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 // Setup __dirname for ES Modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -74,7 +75,7 @@ io.on("connection", (socket) => {
 
 // Serve frontend in production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "frontend", "dist")));
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 	
 	app.get("*", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
