23c2c08e809cbdc0135b5dceed7db28c06419322
Rajan Patel <rajanpatel.ias@gmail.com>
2025-08-10 18:28:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || "*", // Allow Render frontend
+  origin: process.env.CLIENT_URL || true, // Allow Render frontend
   credentials: true
 }));
 app.use(express.json({ limit: "10mb" }));
