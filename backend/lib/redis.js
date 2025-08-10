3300ff4b8124a57b0f4ff6ee4d69c9d06ea68423
Rajan Patel <rajanpatel.ias@gmail.com>
2025-08-10 16:03:34 +0530

    Update redis.js

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -3,5 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const redis = new Redis(process.env.UPSTASH_REDIS_URL);
+export const redis = new Redis(process.env.UPSTASH_REDIS_URL, {
+  maxRetriesPerRequest: null, // Prevents request limit errors
+  tls: {} // Ensures SSL/TLS is enabled for Upstash
+});
 //redis is : key value store
